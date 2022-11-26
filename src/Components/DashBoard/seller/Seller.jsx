689eb23655b473b6629c680c5b1d81acc6363e46
emon <emondas457@gmail.com>
2022-11-26 12:41:14 +0600

    retrive my product

--- src/Components/DashBoard/seller/Seller.jsx
+++ src/Components/DashBoard/seller/Seller.jsx
@@ -1,60 +1,94 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { userContext } from '../../context/AuthProvider';
 const Seller = () => {
+    const { user } = useContext(userContext);
+    const { isLoading, error, data: phones = [] } = useQuery({
+        queryKey: ['Phones'],
+        queryFn: () =>
+            fetch(`http://localhost:5000/getphones?email=${user.email}`).then(res =>
+                res.json()
+            )
+    })
+
     return (
         <div>
-        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
-            <h2 className="mb-4 text-2xl font-semibold leading-tight">All Users</h2>
-            <div className="overflow-x-auto">
-                <table className="min-w-full text-xs">
-                    <colgroup>
-                        <col />
-                        <col />
-                        <col />
+            <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+                <h2 className="mb-4 text-2xl font-semibold leading-tight">My Products</h2>
+                <div className="overflow-x-auto">
+                    <table className="min-w-full text-xs">
+                        <colgroup>
+                           
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
 
-                        <col className="w-24" />
-                    </colgroup>
-                    <thead className="dark:bg-gray-700">
-                        <tr className="text-left">
-                            <th className="p-3">SL</th>
-                            <th className="p-3">Product Name</th>
-                            <th className="p-3">Email</th>
-                            <th className="p-3">Role</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                     
-                                <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                            <col className="w-24" />
+                        </colgroup>
+                        <thead className="dark:bg-gray-700">
+                            <tr className="text-left">
+                                <th className="p-3">SL</th>
+                                <th className="p-3">Image</th>
+                                <th className="p-3">Brand</th>
+                                <th className="p-3">Product Name</th>
+                                <th className="p-3">Original Price</th>
+                                <th className="p-3">Reseal Price</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                phones.map((phone, index) => <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
                                     <td className="p-3">
-                                        <p>index</p>
+                                        <p>{index + 1}</p>
                                     </td>
                                     <td className="p-3">
-                                        <p>user.name</p>
+                                        <div className="avatar">
+                                            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                                <img alt="/" src={phone.image} />
+                                            </div>
+                                        </div>
+                                
                                     </td>
                                     <td className="p-3">
-
-                                        <p className="dark:text-gray-400">user.email</p>
+                                        <p className="dark:text-gray-400">{phone.brand}</p>
                                     </td>
                                     <td className="p-3">
-
-                                        <p className="dark:text-gray-400">user.role</p>
+                                        <p className="dark:text-gray-400">{phone.productName}</p>
+                                    </td>
+                                    <td className="p-3">
+                                        <p className="dark:text-gray-400">$ {phone.originalPrice}</p>
+                                    </td>
+                                    <td className="p-3">
+                                        <p className="dark:text-gray-400">$ {phone.resalePrice}</p>
                                     </td>
 
                                     <td className="p-3 text-right">
                                         <span className="px-3 py-1 font-semibold rounded-md dark:bg-indigo-400 dark:text-gray-900">
-                                            <span>Pending</span>
+                                            <span>Delete</span>
                                         </span>
                                     </td>
-                                </tr>
-                        
+                                    <td className="p-3 text-right">
+                                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-indigo-400 dark:text-gray-900">
+                                            <span>advertise</span>
+                                        </span>
+                                    </td>
+                                </tr>)
+                            }
 
-                    </tbody>
-                </table>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
 
-    </div>
+        </div>
     );
 };
 

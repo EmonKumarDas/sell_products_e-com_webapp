9367ae39ee36655da36a1829c44c850ae6967ea9
emon <emondas457@gmail.com>
2022-11-26 09:27:03 +0600

    add product seller

--- src/Components/DashBoard/seller/AddProductSeller.jsx
+++ src/Components/DashBoard/seller/AddProductSeller.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { userContext } from '../../context/AuthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const AddProductSeller = () => {
     const hostimageKey = process.env.REACT_APP_imgbb_key;
+    const { user } = useContext(userContext);
 
     const handleProduct = (e) => {
         e.preventDefault();
+        const productName = e.target.name.value;
+        const model_role = e.target.model_role.value;
+        const location = e.target.location.value;
+        const r_price = e.target.r_price.value;
+        const o_price = e.target.o_price.value;
+        const uses = e.target.uses.value;
+        const number = e.target.number.value;
+        const description = e.target.uses.value;
         const file = e.target.image.files;
         const image = file[0];
         const formData = new FormData();
-        formData.append('image',image);
+        formData.append('image', image);
         const url = `https://api.imgbb.com/1/upload?expiration=600&key=${hostimageKey}`;
-        fetch(url,{
+        fetch(url, {
             method: 'POST',
-            body:formData
-        }).then(res=>res.json()).then(data=>console.log(data.data.url))
+            body: formData
+        }).then(res => res.json()).then(data => {
+            const SellerData = {
+                seller: user.displayName,
+                brand: model_role,
+                productName,
+                location,
+                resalePrice: r_price,
+                originalPrice: o_price,
+                yearOfUse: uses,
+                number,
+                description,
+                image: data.data.url,
+            }
+            console.log(SellerData);
+            fetch("http://localhost:5000/phones",{
+                method:'POST',
+                headers:{
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(SellerData),
+            })
+                .then(res => res.json())
+                .then(data => {
+                    toast("Data insert successfully")
+                    console.log(data)})
+
+        })
     }
 
     return (
@@ -25,42 +62,52 @@ const AddProductSeller = () => {
                     <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm">
 
                         <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+
+
                             <div className="col-span-full sm:col-span-3">
-                                <label for="firstname" className="text-sm">Product Name</label>
-                                <input id="firstname" name="name" placeholder="Product name" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+                                <select name='model_role' className="select select-bordered w-full max-w-xs">
+                                    <option selected>iphone</option>
+                                    <option>realme</option>
+                                    <option>redmi</option>
+                                </select>
                             </div>
                             <div className="col-span-full sm:col-span-3">
-                                <label for="lastname" className="text-sm">Enter Location</label>
-                                <input id="lastname" name="location" placeholder="Enter Location" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+                                <input type="text" name="name" placeholder="Product name" className="input input-bordered input-secondary w-full max-w-xs" />
+                            </div>
+                            <div className="col-span-full sm:col-span-3">
+
+                                <input type="text" id="lastname" placeholder='Location' name="location" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                             <div className="col-span-full sm:col-span-3">
                                 <input type="file" name="image" className="file-input file-input-bordered w-full max-w-xs" />
+
                             </div>
                             <div className="col-span-full sm:col-span-3">
-                                <label for="email" className="text-sm">Resale Price</label>
-                                <input id="email" name="r_price" placeholder="Resale Price" className="w-full text-white rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+
+                                <input type="text" name="r_price" placeholder="Resale Price" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                             <div className="col-span-full">
-                                <label for="address" className="text-sm">Original Price</label>
-                                <input id="address" name="o_price" placeholder="Original Price" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+
+                                <input type="text" name="o_price" placeholder="Original Price" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                             <div className="col-span-full sm:col-span-2">
-                                <label for="city" className="text-sm">Yerar of use</label>
-                                <input id="city" name="uses" placeholder="Yerar of use" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+
+                                <input type="text" name="uses" placeholder="Yerar of use" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                             <div className="col-span-full sm:col-span-2">
-                                <label for="state" className="text-sm">Phone Number</label>
-                                <input id="state" name="number" placeholder="Phone Number" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+
+                                <input type="text" name="number" placeholder="Phone Number" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                             <div className="col-span-full sm:col-span-2">
-                                <label for="zip" className="text-sm">Description</label>
-                                <input id="zip" name="description" placeholder="description" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400 dark:border-gray-700 dark:text-gray-900" />
+
+                                <input type="text" name="description" placeholder="description" className="input input-bordered input-secondary w-full max-w-xs" />
                             </div>
                         </div>
                     </fieldset>
-                    <button className="btn btn-outline">Button</button>
+                    <button className="btn btn-outline w-96">Button</button>
                 </form>
             </section>
+            <ToastContainer />
         </div>
     );
 };
